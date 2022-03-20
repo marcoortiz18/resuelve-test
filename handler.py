f5b3cbfaae119db9b996d0a5a7876e1edb52b42e
gerard1way <gerardo.acosta.romero.0718@gmail.com>
2022-03-20 16:21:34 -0600

    Making bonus option and dynamic options

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import sys
 
-sys.path.append("../resuelve/backend/Player")
-from resuelve.backend.Player import Player
+sys.path.append("../resuelve/backend/player")
+from resuelve.backend.player import Player
 
 import json
 
@@ -14,4 +14,20 @@ def salary_calculation(event, context):
 
     return calculation
     
+def multi_salary_calculation(event, context):
     
+    request = json.loads(event["body"])
+    
+    player_fc = Player()
+    calculation = player_fc.multi_salary_calculation(request)
+
+    return calculation
+
+def save_json_file(event, context):
+    
+    request = json.loads(event["body"])
+    
+    player_fc = Player()
+    calculation = player_fc.post_levels_json(request)
+
+    return calculation
--- resuelve/backend/Player.py
+++ resuelve/backend/Player.py
@@ -1,6 +1,8 @@
 import sys
 import json
 
+from numpy import full
+
 class Player():
 
     def salary_calculation(self, json_body):
@@ -11,34 +13,45 @@ class Player():
         players_aux = []
         full_percentage = []
 
-        for player in players:
-            player_level = player["nivel"]
-            player_goals = player["goles"]
-            level_goals = Player.get_level(levels, player_level, player_goals)
-            player["goles_minimos"] = level_goals
-            individual_percent = Player.get_individual_percentage(player_goals, level_goals)
-            player["individual_percent"] = individual_percent
-            full_percentage.append(individual_percent)
-            players_aux.append(player)
+        players_aux = Player.set_minimum_goals(players, levels)
+
+        full_percentage = Player.get_full_percentage(players_aux)
 
         team_percentage_avg = Player.get_team_percentage_avg(full_percentage)
         
         for player in players_aux:
             player_complete = Player.get_complete_salary(player, team_percentage_avg)
             players_response.append(player_complete)
-
-        print(players_response)
-
+        
         response = {"statusCode": 200, "body": json.dumps(players_response)}
         return response
 
-    def get_level(levels, player_level,player_goals):
+    def get_levels(json_body):
+        levels = []
+        url_file = "resuelve/backend/levels.json"
+        json_file = open(url_file,"r")
+        levels_data = json.loads(json_file.read())
+        levels = levels_data["niveles"]
+        json_file.close()
+        return levels
+
+    def set_minimum_goals(players, levels):
+        players_with_minimum_goals = []
+        for player in players:
+            player_level = player["nivel"]
+            player_goals = player["goles"]
+            level_goals = Player.get_goals_by_level(levels, player_level)
+            player["goles_minimos"] = level_goals
+            individual_percent = Player.get_individual_percentage(player_goals, level_goals)
+            player["individual_percent"] = individual_percent
+            players_with_minimum_goals.append(player)
+        
+        return players_with_minimum_goals
+
+    def get_goals_by_level(levels, player_level):
         for level in levels:
             if player_level.lower() == level["nivel"].lower():
                 level_goals = level["goles"]
-                #print("player_goals " +str(player_goals))
-                #print("level_goals " +str(level_goals))
-                #print(percent)
 
                 return level_goals
     
@@ -47,6 +60,14 @@ class Player():
         percent = quotient * 100
 
         return percent
+
+    def get_full_percentage(players):
+        full_percentage = []
+        for player in players:
+            individual_percent = player["individual_percent"]
+            full_percentage.append(individual_percent)
+
+        return full_percentage
 
     def get_team_percentage_avg(full_percentage):
         team_percentage = 0
@@ -76,4 +97,54 @@ class Player():
         
         return player
     
-    
+    def multi_salary_calculation(self, json_body):
+        keys = json_body.keys()
+
+        multi_players = []
+        index = 0
+
+        for key in keys:
+            players = []
+            
+            players = json_body[key]
+            player = Player.salary_calculations(self, players)
+            player = {""+ str(key) + ":": player}
+            
+            multi_players.append(player)
+            index += 1
+        
+
+
+        response = {"statusCode": 200, "body": json.dumps(multi_players)}
+        return response
+
+    def salary_calculations(self, json_body):
+        players = json_body
+        levels = Player.get_levels(json_body)
+        
+        players_response = []
+        players_aux = []
+        full_percentage = []
+
+        players_aux = Player.set_minimum_goals(players, levels)
+
+        full_percentage = Player.get_full_percentage(players_aux)
+
+        team_percentage_avg = Player.get_team_percentage_avg(full_percentage)
+        
+        for player in players_aux:
+            player_complete = Player.get_complete_salary(player, team_percentage_avg)
+            players_response.append(player_complete)
+        
+        return players_response
+
+    def post_levels_json(self,json_levels):
+        url_file = "resuelve/backend/levels.json"
+        json_file = open(url_file,"r+")
+        json_file.truncate(0)
+        json.dump(json_levels, json_file)
+        json_file.close()
+
+        response = {"statusCode": 200, "body": "{\"status\": \"Saved!\"}"}
+
+        return response
